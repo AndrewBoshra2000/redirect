e3ebe1d3c20e9ba46b0ea911c7978fbf4625166d
Andrew Boshra <andrew.boshra@reno.systems>
2023-04-12 17:06:52 +0200

    added proxy server

--- app.js
+++ app.js
@@ -44,7 +44,32 @@ app.get("/", async (req, res) => {
 
     res.send(getPage());
 });
+
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const splunkUrl = "http://splunk.getmedic.co:8088/services/collector";
+app.use(
+    "/splunk",
+    createProxyMiddleware({
+        target: splunkUrl,
+        changeOrigin: false,
+
+        followRedirects: true,
+        logLevel: "debug",
+        pathRewrite: {
+            "^/splunk": "",
+        },
+        headers: {
+            Authorization: "Splunk 90d4d19e-6eac-4edb-b414-09b43949175e",
+        },
+        onProxyRes: function (proxyRes, req, res) {
+            proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+            proxyRes.headers["Access-Control-Allow-Headers"] = "Authorization";
+        },
+    })
+);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port http://localhost:${port}/`);
 });
